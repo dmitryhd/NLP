33800bf5e626561df8f7052a807696ee86c440d4
Dmitry Khodakov <dmitryhd@google.com>
2013-04-28 17:55:04 +0400

    xx

--- Analyser.py
+++ Analyser.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
 import glob
@@ -8,6 +8,70 @@ import sys
 import operator
 from subprocess import Popen, PIPE
 from DataBase import *
+
+
+min_cnt = 1 # minimum number of word in document for word to be considered
+max_words = 100 # maximum number of words in frequency dictionary
+
+def classify_articles (articles_file_names,
+             db_name_a='data_bases/sciAm2008-2011.db',
+             db_name_b='data_bases/fiction.db'):
+    """ bayesian classify given articles to class A, or B"""
+    total_word_prob_a = GetTotalWordProb(db_name_a)
+    articles_a = GetAllArticlesNormalized (db_name_a)
+    total_word_prob_b = GetTotalWordProb(db_name_b)
+    articles_b = GetAllArticlesNormalized (db_name_b)
+    for fname in articles_file_names:
+        print("classify article: {}".format(fname))
+        with open('data/' + fname, 'r') as fd:
+            text = fd.read()
+        norm_fd = NormalizeDict(GetFrequencyDict(NormalizeText(text)))
+        prob_text_belong_to_a = BayesProb(norm_fd, total_word_prob_a)
+        prob_text_belong_to_b = BayesProb(norm_fd, total_word_prob_b)
+        value = 0
+        if prob_text_belong_to_a > prob_text_belong_to_b:
+            value = prob_text_belong_to_a/prob_text_belong_to_b
+            print ('article %30s belongs to class A, p_a/p_b = %e' % (fname, value))
+        else:
+            value = prob_text_belong_to_b/prob_text_belong_to_a
+            print ('article %30s belongs to class B, p_b/p_a = %e' % (fname, value))
+
+
+# -------------- bayes
+def GetTotalWordProb (dbname):
+    """ for every word in cluster: get prob. that word is in document) """
+    sqlOpRead = SQLOperator(dbname)
+    articles = sqlOpRead.ReadAllArticles()
+    total_dict = GetTotalFreqDict ([article.freqDict for article in articles])
+    total_word_prob = {}
+    for article in articles:
+        for word in article.freqDict.keys():
+            if (word not in total_word_prob.keys()):
+                total_word_prob[word] = 1
+            else:
+                total_word_prob[word] += 1
+                total_word_prob[word] = int(total_word_prob[word])
+    for word in total_word_prob.keys():
+        if total_word_prob[word] == 0:
+            continue
+        total_word_prob[word] = float (total_word_prob[word])/len(articles)
+        assert total_word_prob[word] # not equals 0
+    PrintFancyDict(total_word_prob, 10000)
+    return total_word_prob
+
+
+def BayesProb (art_norm_fd, total_word_prob):
+    min_prob = min(total_word_prob.values())
+    p = 1
+    if not min_prob:
+        min_prob = 0.1
+    for word in art_norm_fd:
+        if word not in total_word_prob:
+            p *= min_prob
+        else:
+            p *= total_word_prob[word]
+    return p
+
 
 # ------------- functions to prepare parsed articles for analysis
 def IntercectDicts (listOfDicts):
@@ -21,11 +85,11 @@ def IntercectDicts (listOfDicts):
     return res
 
 
-def PrintFancyDict (freq_dict, max_cnt = 20):
+def PrintFancyDict (freq_dict, max_cnt = 100):
     x = sorted(freq_dict.items(), key=operator.itemgetter(1), reverse=True)
     cnt = 0
     for i in x:
-        print ("%11s %.5f" % (i[0], i[1]))
+        print ("%50s %.5f" % (i[0], i[1]))
         if cnt > max_cnt:
             return
         cnt += 1
@@ -54,16 +118,27 @@ def GetTotalFreqDict (list_of_norm_dicts):
         i += 1
     return res
 
+def NormalizeDict (freq_dict):
+    """ get only most valuable words, if wc = 1 - ignore """
+    total_cnt = 0
+    for key in freq_dict.keys():
+        total_cnt += freq_dict[key]
+    normalized_dict = {}
+    for key in freq_dict.keys():
+        if freq_dict[key] < min_cnt:
+            continue
+        normalized_dict[key] = float(freq_dict[key])/total_cnt
+    # normalized_dict
+    res = {}
+    cnt = 0
+    for w in sorted(normalized_dict, key=normalized_dict.get, reverse=True):
+        if cnt == max_words:
+            break
+        res[w] = normalized_dict[w]
+        cnt += 1
+    return res
 
 def GetAllArticlesNormalized(dbname):
-    def NormalizeDict (freq_dict):
-        total_cnt = 0
-        for key in freq_dict.keys():
-            total_cnt += freq_dict[key]
-        normalized_dict = {}
-        for key in freq_dict.keys():
-            normalized_dict[key] = float(freq_dict[key])/total_cnt
-        return normalized_dict
     sqlOpRead = SQLOperator(dbname)
     articles = sqlOpRead.ReadAllArticles()
     normalized_articles = {article.name:NormalizeDict(article.freqDict) for article in articles}
@@ -124,6 +199,28 @@ def NormalizeText(text):
         text = rfd.read()
     os.chdir(cwd)
     return text.strip()
+
+
+def GetSeparateTextsFromTXT (fiction_dir='data/fiction/',
+                             db_path='data_bases/fiction.db'):
+    """ Get name of directory full of text files, create articles for them
+        and store to given db file"""
+    articles = []
+    article_id = 0
+    current_dir = os.getcwd()
+    os.chdir(fiction_dir)
+    for file_name in glob.glob('*.txt'):
+        with open(file_name, 'r') as fd:
+            text = fd.read()
+        norm_text = NormalizeText(text)
+        print("******* preparing article from {}".format(file_name))
+        article = Article(article_id, "fic", file_name, text,
+                          norm_text, GetFrequencyDict(norm_text))
+        article_id += 1
+        articles.append(article)
+    os.chdir(current_dir)
+    SaveArticlesToDB(articles, db_path)
+    return articles
 
 
 def GetSeparateTextsFromPDF(directory):
@@ -248,8 +345,16 @@ if __name__ == '__main__':
     # AnalyzeTexts('data_sci')
     # print("total: {} keywords".format(len(total_dict)))
     # PrintFancyDict(total_dict, 100)
-    norm_articles = GetAllArticlesNormalized(sys.argv[1])
-    for a in norm_articles.keys():
-        print ("{} ->".format(a))
-        PrintFancyDict (norm_articles[a],15)
-        print ("==================")
+#    norm_articles = GetAllArticlesNormalized(sys.argv[1])
+#    for a in norm_articles.keys():
+#        print ("{} ->".format(a))
+#        PrintFancyDict (norm_articles[a],15)
+#        print ("==================")
+    #main(sys.argv[1])
+    to_compare = [
+                    'A_Christmas_Carol-Charles_Dickens.txt',
+                    'A_Descent_Into_the_Maelstrom-Edgar_Allan_Poe.txt'
+            ]
+    classify_articles(to_compare)
+    #main('data_bases/sciAm2008-2011.db')
+    #GetSeparateTextsFromTXT('data/fiction','data_bases/fiction.db')
