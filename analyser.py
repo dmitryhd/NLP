47ffe0fcdd0b1488c3711a9434500ec823115b24
Dmitry Khodakov <dmitryhd@google.com>
2013-05-14 11:04:26 +0400

    dont work

--- analyser.py
+++ analyser.py
@@ -11,6 +11,8 @@ import operator
 from subprocess import Popen, PIPE
 import sqlite3
 import pickle
+import nltk
+from threading import Thread
 
 
 MinCnt = 1  # minimum number of word in document for word to be considered
@@ -51,8 +53,11 @@ def SaveArticles (db_name, articles):
     conn.cursor().execute(
             'CREATE TABLE IF NOT EXISTS articles (id text, atype text, name text, txt text, normtxt text, freqdict text)')
     for article in articles:
-        conn.cursor().execute("INSERT INTO articles VALUES (?,?,?,?,?,?)", (str(
-            article.index), article.atype, article.name, article.text, article.normtext, pickle.dumps(article.freq_dict)))
+        try:
+            conn.cursor().execute("INSERT INTO articles VALUES (?,?,?,?,?,?)", (unicode(str(
+            article.index)), unicode(article.atype), unicode(article.name), unicode(article.text), unicode(article.normtext), pickle.dumps(article.freq_dict)))
+        except:
+            print("fail... {}".format(article.name))
     conn.commit()
     conn.close()
 
@@ -64,7 +69,7 @@ def ReadAllArticles (db_name):
     try:
         query_result = conn.cursor().execute('SELECT * FROM articles ORDER BY atype')
     except:
-        print 'cannot read from this database {}'.format(db_name)
+        print('cannot read from this database {}'.format(db_name))
         return []
     for row in query_result:
         article = Article(row[0], row[1], row[
@@ -75,13 +80,44 @@ def ReadAllArticles (db_name):
     return db_content
 
 
-def CreateBdOfArticles(directory_name, db_name):
+def GetArticlesFromDir(directory_name):
+    """ return articles made from pdf or txt files in this directory"""
+    def run_in_parallel(functions, arg=None):
+        """ Should run multiple functions in thread, and wait for all of them 
+            arg is list of list
+        """
+        threads = []
+        index = 0
+        for function in functions:
+            if arg:
+                thread = Thread(target=function, args=arg[index])
+            else:
+                thread = Thread(target=function)
+            thread.start()
+            threads.append(thread)
+            index += 1
+        for thread in threads:
+            thread.join()
+    text_files = []
+    articles = []
+
+    def create_article_from_text (index, text):
+        """ thread function, save article in articles variable at index place """
+        print("thread {}, file: {}".format(index, text_files[index]))
+        norm_text = NormalizeText(text)
+        article = Article(index, "fic", text_files[index], text,
+                          norm_text, GetFrequencyDict(norm_text))
+        articles[index] = article
+
     cwd = os.getcwd()
     os.chdir(directory_name)
+    text_files = glob.glob('*.pdf')
+    text_files.extend(glob.glob('*.txt'))
+    print(text_files)
+    args = [] # multple texts
     article_id = 0
-    articles = []
-    for fname in glob.glob('*.pdf'):
-        print "classify article: {}".format(fname)
+    for fname in text_files:
+        print("classify article: {}".format(fname))
         file_name, file_extension = os.path.splitext(fname)
         if file_extension == '.pdf':
             pdftotextcommand = ['pdftotext', '-enc', 'UTF-8']
@@ -94,13 +130,28 @@ def CreateBdOfArticles(directory_name, db_name):
         else:
             with open(fname, 'r') as fd:
                 text = fd.read()
-        norm_text = NormalizeText(text)
-        article = Article(article_id, "fic", file_name, text,
-                          norm_text, GetFrequencyDict(norm_text))
-        articles.append(article)
+        args.append([article_id, text])
         article_id += 1
-    os.chdir(cwd)    # save to Data Base
-    SaveArticles(db_name, articles)
+    articles = [None] * article_id
+    run_in_parallel([create_article_from_text]*article_id, args)
+    os.chdir(cwd)
+    return articles
+
+
+def GetArticlesFromDirRecursively(directory):
+    """ recusively get all text from given directory """
+    currentwd = os.getcwd()
+    print("cd to {}".format(directory))
+    os.chdir(directory)
+    articles = []
+    for root, dirs, files in os.walk('.'):
+        print('dirs: {}'.format(dirs))
+        articles.extend(GetArticlesFromDir('.'))
+        for current_directory in dirs:
+            print("get data fom {}".format(current_directory))
+            articles.extend(GetArticlesFromDir(current_directory))
+    os.chdir(currentwd)
+    return articles
 
 
 def ClassifyArticles(articles_db_filename,
@@ -118,8 +169,7 @@ def ClassifyArticles(articles_db_filename,
             calculated_class = 'A'
         else:
             calculated_class = 'B'
-        print 'article %30s belongs to class %s, p_a %e p_b %e' %
-                 (article.name, calculated_class, prob_text_belong_to_a, prob_text_belong_to_b)
+        print('article {:30} belongs to class {}, p_a {} p_b {}'.format(article.name, calculated_class, prob_text_belong_to_a, prob_text_belong_to_b))
 
 
 def GetTotalWordProb(dbname):
@@ -172,31 +222,30 @@ def PrintFancyDict(freq_dict, max_cnt=100):
         freq_dict.items(), key=operator.itemgetter(1), reverse=True)
     cnt = 0
     for item in sorted_dict:
-        print "%50s %.5f" % (item[0], item[1])
+        print("{:50} {}".format(item[0], item[1]))
         if cnt > max_cnt:
             return
         cnt += 1
 
 
 def NormalizeText(text):
-    """ Get text to normal form. Using python2 and nltk. WARNING: uncompatible! """
+    """ Get text to normal form. Using python2 and nltk. """
     def GetTextFromSting(string):
         """ Get only plain english text with spaces """
         return (''.join(c if c.isupper() or c.islower() else ' ' for c in string)).lower()
-    main_wd = '/home/dimert/repos/NLP'
-    cwd = os.getcwd()
-    os.chdir(main_wd)
     # tokenizing
     text = GetTextFromSting(text)
-    with open("exchange_inp.txt", 'w') as wfd:
-        wfd.write(text)
-    os.system('python2 normalize.py')
-    with open("exchange_out.txt", 'r') as rfd:
-        text = rfd.read()
-    os.remove("exchange_out.txt")
-    os.remove("exchange_inp.txt")
-    os.chdir(cwd)
-    return text.strip()
+    # use stemmer 
+    st = nltk.stem.lancaster.LancasterStemmer()
+
+    goodPOS = ('FW', 'JJ', 'JJR', 'JJS', 'NN', 'NNP', 'NNPS', 'NNS', 'RB', 'VB', "VBD", "VBG", "VBN", "VBP", "VBZ")
+
+    toc = nltk.word_tokenize(text)
+    pos = nltk.pos_tag(toc)
+
+    # forbrid some parts of speech
+    normtext = [st.stem(p[0]) for p in pos if len(p[0]) > 3 and p[1] in goodPOS]
+    return ' '.join(normtext).strip()
 
 
 def GetFrequencyDict(rawtext):
@@ -235,75 +284,13 @@ def GetFrequencyDict(rawtext):
     return most_freq_dict
 
 
-def GetSeparateTextsFromTXT(fiction_dir='data/fiction/',
-                            db_path='data_bases/fiction.db'):
-    """ Get name of directory full of text files, create articles for them
-        and store to given db file"""
-    articles = []
-    article_id = 0
-    current_dir = os.getcwd()
-    os.chdir(fiction_dir)
-    for file_name in glob.glob('*.txt'):
-        with open(file_name, 'r') as fd:
-            text = fd.read()
-        norm_text = NormalizeText(text)
-        print "******* preparing article from {}".format(file_name)
-        article = Article(article_id, "fic", file_name, text,
-                          norm_text, GetFrequencyDict(norm_text))
-        article_id += 1
-        articles.append(article)
-    os.chdir(current_dir)
-
-    # save to Data Base
-    SaveArticles(db_name, articles)
-
-
-def GetSeparateTextsFromPDF(directory, db_path='data_bases/sciAm2008-2011.db'):
-    """ recusively get all text from given directory """
-    def CreateArticlesFromThisDirectory(begin_id=0):
-        """ get all texts from current directory, return list of articles"""
-        pdftotextcommand = ['pdftotext', '-enc', 'UTF-8']
-        articles = []
-        articleid = begin_id
-        for filename in glob.glob('*.pdf'):
-            command = pdftotextcommand + [filename, 'tmp']
-            process = Popen(command, stdout=PIPE)
-            os.waitpid(process.pid, 0)
-            with open('tmp', 'r') as fd:
-                text = fd.read()
-                norm_text = NormalizeText(text)
-                print "******* preparing article from {}".format(filename)
-                article = Article(
-                    articleid, "sci", filename, text, norm_text, GetFrequencyDict(norm_text))
-                articles.append(article)
-            os.remove('tmp')
-            articleid += 1
-        return articles
-
-    os.chdir(directory)
-    currentwd = os.getcwd()
-    result = CreateArticlesFromThisDirectory()
-    article_id = len(result)
-    for innerdirectory in [directory[1] for directory in os.walk('.')]:
-        if not innerdirectory:
-            continue
-        innerdirectory = innerdirectory[0]
-        try:
-            os.chdir(innerdirectory)
-        except:
-            print "can't cd dir to {}".format(innerdirectory)
-            continue
-        result.append(CreateArticlesFromThisDirectory(article_id))
-        article_id = len(result)
-        os.chdir(currentwd)
-    os.chdir("..")
-    # save to Data Base
-    SaveArticles(db_name, articles)
-
-
 if __name__ == '__main__':
     #ClassifyArticles(to_compare)
-    #CreateBdOfArticles('.','test_sample.db')
-    ClassifyArticles('test_sample.db', 'data_bases/sci_articles.db')
+#    articles = GetArticlesFromDir('.')
+    #import sys
+    #articles = GetArticlesFromDirRecursively(sys.argv[1])
+    #SaveArticles(sys.argv[2], articles)
+    #ClassifyArticles('test_sample_2.db', 'data_bases/fiction_2.db', 'data_bases/science_2.db')
+    ClassifyArticles('test_sample.db', 'data_bases/fiction.db', 'data_bases/sci_article.db')
 #    total_word_prob_a = GetTotalWordProb('data_bases/sciAm2008-2011.db')
 #    total_word_prob_b = GetTotalWordProb('data_bases/fiction.db')
